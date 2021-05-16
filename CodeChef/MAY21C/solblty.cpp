9b2cd3f1a46a56acdd6748f882bfb9589844df15
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-16 16:53:54 +0530

    Create solblty.cpp

---
+++ CodeChef/MAY21C/solblty.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t,x,a,b;
+	cin>>t;
+	while (t--) {
+		
+		cin>>x>>a>>b;
+		cout<<((100-x)*b+a)*10<<"\n";
+		}
+	}
