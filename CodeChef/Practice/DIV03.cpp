95f097ce0b8c65d2480f908098421e6bb40d02cb
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-20 22:46:38 +0530

    Create DIV03.cpp

---
+++ CodeChef/Practice/DIV03.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t,sum=0,k;
+	int a[10];
+	cin>>t;
+	while(t--) {
+		sum=0;
+		for(int i=0;i<10;i++)
+			cin>>a[i];
+		cin>>k;
+		for(int i=9;i>=0;i--) {
+			
+			sum+=a[i];
+			if(sum>k) {
+				cout<<i+1<<endl;
+				break;
+				}
+			}
+		}
+	return 0;
+	}
+	
+			
