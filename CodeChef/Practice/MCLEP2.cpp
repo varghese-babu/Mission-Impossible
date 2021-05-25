7f8c3ad9092e8a5c2a5b56a257f355af4b7fb34f
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-25 21:40:43 +0530

    Create MCLEP2.cpp

---
+++ CodeChef/Practice/MCLEP2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int d,t;
+	cin>>d>>t;
+	if(d/t<30)
+		cout<<"FIRED"<<endl;
+	else
+		cout<<"NOT FIRED"<<endl;
+	return 0;
+	}
