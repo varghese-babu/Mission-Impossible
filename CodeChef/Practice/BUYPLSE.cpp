62e05fe513fd447ba21f0234b8282aa0936f88e2
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-28 21:37:44 +0530

    Create BUYPLSE.cpp

---
+++ CodeChef/Practice/BUYPLSE.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	cout<<(a*x)+(b*y);
+	return 0;
+	}
