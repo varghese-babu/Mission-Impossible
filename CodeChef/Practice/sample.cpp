b84782149500d7c9b727cae425c871a8c833600c
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-29 22:41:17 +0530

    Create sample.cpp

---
+++ CodeChef/Practice/sample.cpp
@@ -0,0 +1,14 @@
+  #include <bits/stdc++.h>
+    using namespace std;
+    typedef long long ll;
+     
+    int main() {
+        ios_base::sync_with_stdio(false); cin.tie(0);
+        int a,b;
+        cin>>a>>b;
+        if(a>=30 || b>=3)
+          cout<<"QUALIFIED";
+        else
+          cout<<"NOT QUALIFIED";
+        return 0;
+        }
