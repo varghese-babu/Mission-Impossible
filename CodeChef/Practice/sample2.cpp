5aecb20632a80d79c2bad4da34c58a6b1ac6a268
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-05-31 22:39:01 +0530

    Create sample2.cpp

---
+++ CodeChef/Practice/sample2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+   ios_base::sync_with_stdio(false); cin.tie(0);
+   int t,n;
+   cin>>t;
+   while(t--) {
+ 
+       cin>>n;
+       cout<<n*4+1<<endl;
+       }
+   return 0;
+   }
