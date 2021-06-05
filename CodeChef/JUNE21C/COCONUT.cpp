1fbd3152f5651bb54022d4568f89f74f41b7451b
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-06-05 23:11:28 +0530

    Create COCONUT.cpp

---
+++ CodeChef/JUNE21C/COCONUT.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--) {
+		int a,b,A,B;
+		cin>>a>>b>>A>>B;
+		cout<<(A/a)+(B/b)<<endl;
+		}
+	return 0;
+	}
