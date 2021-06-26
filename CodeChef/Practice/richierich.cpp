7433d41fd09ef422ce2fa4c78815a69aa116546e
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-06-26 22:47:05 +0530

    Add files via upload

---
+++ CodeChef/Practice/richierich.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--) {
+		
+		int a,b,x;
+		cin>>a>>b>>x;
+		cout<<(b-a)/x<<endl;
+		}
+	return 0;
+	}
