ba8e6d7c41f97c03d1c5e0880c1ea915ff27487e
varghese-babu <55503677+varghese-babu@users.noreply.github.com>
2021-06-27 18:51:36 +0530

    Add files via upload

---
+++ CodeChef/JuneStarters/chessmatch.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    int t;
+    cin>>t;
+    while(t--) {
+    	
+    	int n,a,b;
+    	cin>>n>>a>>b;
+    	cout<<(2*(180+n)-(a+b))<<endl;
+    	}
+    return 0;
+    }
